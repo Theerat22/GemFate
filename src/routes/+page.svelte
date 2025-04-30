28a5683f204c5bc6127f39f71d02abc2d0d55df1
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2025-05-01 01:00:49 +0700

    responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,45 @@
-
-
-
-<div class="flex min-h-screen max-w-screen flex-col bg-[url(/bg-result.png)] bg-cover bg-no-repeat">
-	<div class="flex justify-between overflow-hidden">
-		<img src="logo.png" /> <img src="itCamp.png" class="m-10" />
-	</div>
-	<div class="flex flex-col items-center justify-center mt-20">
-		<img src="gemfatetitle.png" class="scale-150"/>
-		<a href="random" class="m-auto mt-50"
-			><img src="button.png" class="scale-100 duration-500 ease-in-out hover:scale-125" /></a
+<script>
+	// You can add any necessary Svelte logic here
+	import { onMount } from 'svelte';
+	
+	function handleRandomClick() {
+	  window.location.href = '/random';
+	}
+  </script>
+  
+  <svelte:head>
+	<title>Responsive Page</title>
+  </svelte:head>
+  
+  <section class="relative min-h-screen overflow-hidden">
+	<div class="flex flex-col bg-cover bg-no-repeat min-h-screen" 
+		 style="background-image: url('/bg-result.png')">
+	  
+	  <!-- Header with logos -->
+	  <div class="flex justify-between items-center px-4 py-4 sm:px-10">
+		<img src="logo.png" alt="Logo" class="h-20 sm:h-20" />
+		<img src="itCamp.png" alt="IT Camp" class="h-12 sm:h-16" />
+	  </div>
+	  
+	  <!-- Center Content -->
+	  <div class="flex flex-col items-center justify-center flex-grow px-1 7">
+		<img src="gemfatetitle.png" alt="Gem Fate Title" class="w-3/4 sm:w-2/4 lg:w-1/3 mb-10" />
+		<button 
+		  on:click={handleRandomClick} 
+		  class="transition-transform duration-500 ease-in-out hover:scale-125 bg-transparent border-none p-0 cursor-pointer"
 		>
+		  <img src="button.png" alt="Start Button" class="w-40 sm:w-48 lg:w-56" />
+		</button>
+	  </div>
+  
 	</div>
-</div>
+  </section>
+  
+  <!-- 
+	Optional: Add global styles
+	In Svelte you can add styles here that will be scoped to this component
+  -->
+  <style>
+	/* Add any additional component-specific styles here */
+	/* These styles will be processed by Svelte and scoped to this component */
+  </style>
--- src/routes/result/+page.svelte
+++ src/routes/result/+page.svelte
@@ -43,24 +43,41 @@
 	});
 </script>
 
-<div
-	class="flex min-h-screen flex-col items-center justify-center bg-[url('/bg-result.png')] bg-cover bg-no-repeat"
->
-	<div class="flex flex-col p-5 text-center">
-		<!-- <h1 class="mb-10 font-bold text-3xl">Upload Image</h1> -->
+<section class="relative min-h-screen overflow-hidden">
+	<div class="flex min-h-screen flex-col items-center justify-center bg-[url('/bg-result.png')] bg-cover bg-no-repeat">
+		<div class="flex flex-col p-5 text-center">
+			<!-- <h1 class="mb-10 font-bold text-3xl">Upload Image</h1> -->
 
-		{#if avatar}
-			<!-- <canvas
-				id="canvas"
-				width="1000px"
-				height="500px"
-				class="z-0 h-[500px] w-[300px] items-center rounded-2xl bg-white px-8 pt-9"
-			></canvas> -->
+			{#if avatar}
+				<!-- <canvas
+					id="canvas"
+					width="1000px"
+					height="500px"
+					class="z-0 h-[500px] w-[300px] items-center rounded-2xl bg-white px-8 pt-9"
+				></canvas> -->
+				<div class="m-auto mt-25 h-[700px] w-[500px] bg-[#FFD3D3]">
+					<div class="flex justify-center">
+						<img src="gemfatephoto.png" />
+					</div>
+					<img class="m-auto h-[386px] w-[404px] object-cover " src={avatar} alt="name">
+					<div class="m-auto flex justify-center">
+						<img src="icon.png" class="m-5" />
+					</div><img src="photogem1.png" class="translate-x-[400px] -translate-y-[500px]" />
+					<img src="photogem2.png" class="-translate-x-[50px] -translate-y-[450px]" />
+					<img src="photogem3.png" class="translate-x-[470px] -translate-y-[325px]" />
+					<img src="photogem4.png" class="-translate-x-[50px] -translate-y-[200px]" />
+
+					<p class="-translate-y-[300px]  text-xl font-prompt">{full_sentence_local}</p>
+				</div>
+
+				<!-- Download Complete -->
+				<!-- <button onclick={putImage} class="bg-pink-300 p-3">Click to console</button> -->
+			{:else}
 			<div class="m-auto mt-25 h-[700px] w-[500px] bg-[#FFD3D3]">
 				<div class="flex justify-center">
 					<img src="gemfatephoto.png" />
 				</div>
-				<img class="m-auto h-[386px] w-[404px] object-cover " src={avatar} alt="name">
+				<div class="m-auto h-[386px] w-[404px] bg-white"></div>
 				<div class="m-auto flex justify-center">
 					<img src="icon.png" class="m-5" />
 				</div><img src="photogem1.png" class="translate-x-[400px] -translate-y-[500px]" />
@@ -71,42 +88,25 @@
 				<p class="-translate-y-[300px]  text-xl font-prompt">{full_sentence_local}</p>
 			</div>
 
-			<!-- Download Complete -->
-			<!-- <button onclick={putImage} class="bg-pink-300 p-3">Click to console</button> -->
-		{:else}
-		<div class="m-auto mt-25 h-[700px] w-[500px] bg-[#FFD3D3]">
-			<div class="flex justify-center">
-				<img src="gemfatephoto.png" />
-			</div>
-			<div class="m-auto h-[386px] w-[404px] bg-white"></div>
-			<div class="m-auto flex justify-center">
-				<img src="icon.png" class="m-5" />
-			</div><img src="photogem1.png" class="translate-x-[400px] -translate-y-[500px]" />
-			<img src="photogem2.png" class="-translate-x-[50px] -translate-y-[450px]" />
-			<img src="photogem3.png" class="translate-x-[470px] -translate-y-[325px]" />
-			<img src="photogem4.png" class="-translate-x-[50px] -translate-y-[200px]" />
-
-			<p class="-translate-y-[300px]  text-xl font-prompt">{full_sentence_local}</p>
+			<button
+			class="mt-10 m-auto duration-500 ease-in-out hover:scale-125"
+			onclick={() => {
+				fileinput.click();
+			}}>
+			<img src="/add.png" alt="add">
+			</button
+	>
+	<input
+		style="display:none"
+		type="file"
+		accept=".jpg, .jpeg, .png"
+		onchange={(e) => onFileSelected(e)}
+		bind:this={fileinput}
+	/>
+			{/if}
 		</div>
-
-		<button
-		class="mt-10 m-auto duration-500 ease-in-out hover:scale-125"
-		onclick={() => {
-			fileinput.click();
-		}}>
-		<img src="/add.png" alt="add">
-		</button
->
-<input
-	style="display:none"
-	type="file"
-	accept=".jpg, .jpeg, .png"
-	onchange={(e) => onFileSelected(e)}
-	bind:this={fileinput}
-/>
-		{/if}
 	</div>
-</div>
+</section>
 
 
 <!-- <button
